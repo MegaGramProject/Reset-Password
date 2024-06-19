7c16f7e92ad08d556d0ccade9ace12854db381a4
Rishav Ray <risjavram@gmail.com>
2024-06-19 09:07:09 -0500

    small adjustments

--- springBootBackend1/src/main/java/com/example/springBootBackend1/backendController.java
+++ springBootBackend1/src/main/java/com/example/springBootBackend1/backendController.java
@@ -25,18 +25,13 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class backendController {
@@ -206,16 +201,17 @@ public class backendController {
         try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword)) {
             if (request.containsKey("username")) {
                 String username = request.get("username");
-                String authToken = generateToken(113);
-                String refreshToken = generateToken(113);
-
+                String authToken = generateToken(100);
+                String refreshToken = generateToken(100);
+                String authTokenSalt = generateToken(32);
+                String refreshTokenSalt = generateToken(32);
                 String authTokenExpiry = getExpirationDate(45);
                 String refreshTokenExpiry = getExpirationDate(4320);
+                String hashedAuthToken = hashToken(authToken + authTokenSalt);
+                String hashedRefreshToken = hashToken(refreshToken + refreshTokenSalt);
 
-                String hashedAuthToken = hashToken(authToken);
-                String hashedRefreshToken = hashToken(refreshToken);
-
-                updateDatabase(connection, username, hashedAuthToken, hashedRefreshToken, authTokenExpiry,
+                updateDatabase(connection, username, hashedAuthToken, hashedRefreshToken, authTokenSalt,
+                        refreshTokenSalt, authTokenExpiry,
                         refreshTokenExpiry);
                 return "{\"token\": \"" + authToken + "\", \"refreshToken\": \"" + refreshToken + "\"}";
             } else {
@@ -240,14 +236,17 @@ public class backendController {
     }
 
     private void updateDatabase(Connection connection, String username, String hashedAuthToken,
-            String hashedRefreshToken, String authTokenExpiry, String refreshTokenExpiry) throws Exception {
-        String sql = "INSERT INTO userTokens (username, authToken, refreshToken, authTokenExpiry, refreshTokenExpiry) VALUES (?, ?, ?, ?, ?)";
+            String hashedRefreshToken, String authTokenSalt, String refreshTokenSalt, String authTokenExpiry,
+            String refreshTokenExpiry) throws Exception {
+        String sql = "INSERT INTO userTokens (username, hashedAuthToken, hashedRefreshToken, authTokenSalt, refreshTokenSalt, authTokenExpiry, refreshTokenExpiry) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setString(1, username);
             pstmt.setString(2, hashedAuthToken);
             pstmt.setString(3, hashedRefreshToken);
-            pstmt.setString(4, authTokenExpiry);
-            pstmt.setString(5, refreshTokenExpiry);
+            pstmt.setString(4, authTokenSalt);
+            pstmt.setString(5, refreshTokenSalt);
+            pstmt.setString(6, authTokenExpiry);
+            pstmt.setString(7, refreshTokenExpiry);
             pstmt.executeUpdate();
         }
     }
@@ -260,28 +259,68 @@ public class backendController {
         return sdf.format(expiryDate);
     }
 
-    @GetMapping("/set-tokens")
-    @CrossOrigin(origins = "http://localhost:8000")
-    public String setTokens(HttpServletResponse response, @RequestHeader("Auth-Token") String authToken,
-            @RequestHeader("Refresh-Token") String refreshToken) {
-        ResponseCookie authTokenCookie = ResponseCookie.from("authToken", authToken)
-                .httpOnly(true)
-                .secure(false)
-                .path("/")
-                .maxAge(10 * 365 * 24 * 60 * 60)
-                .build();
+    /*
+     * @GetMapping("/set-tokens")
+     * 
+     * @CrossOrigin(origins = "http://localhost:8000", allowedHeaders = "*",
+     * allowCredentials = "true")
+     * public String setTokens(HttpServletResponse
+     * response, @RequestHeader("Auth-Token") String authToken,
+     * 
+     * @RequestHeader("Refresh-Token") String
+     * refreshToken, @RequestHeader("Username") String username) {
+     * ResponseCookie authTokenCookie = ResponseCookie.from("authToken" + username,
+     * authToken)
+     * .httpOnly(true)
+     * .secure(false)
+     * .path("/")
+     * .maxAge(10 * 365 * 24 * 60 * 60)
+     * .build();
+     * 
+     * ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken" +
+     * username, refreshToken)
+     * .httpOnly(true)
+     * .secure(false)
+     * .path("/")
+     * .maxAge(10 * 365 * 24 * 60 * 60)
+     * .build();
+     * 
+     * response.addHeader("Set-Cookie", authTokenCookie.toString());
+     * response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+     * 
+     * return "HttpOnly cookies set";
+     * }
+     * 
+     * @GetMapping("/unset-tokens")
+     * 
+     * @CrossOrigin(origins = "http://localhost:8000", allowedHeaders = "*",
+     * allowCredentials = "true")
+     * public String unsetTokens(HttpServletResponse
+     * response, @RequestHeader("Auth-Token") String authToken,
+     * 
+     * @RequestHeader("Refresh-Token") String
+     * refreshToken, @RequestHeader("Username") String username) {
+     * ResponseCookie authTokenCookie = ResponseCookie.from("authToken" + username,
+     * "")
+     * .httpOnly(true)
+     * .secure(false)
+     * .path("/")
+     * .maxAge(0)
+     * .build();
+     * 
+     * ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken" +
+     * username, "")
+     * .httpOnly(true)
+     * .secure(false)
+     * .path("/")
+     * .maxAge(0)
+     * .build();
+     * 
+     * response.addHeader("Set-Cookie", authTokenCookie.toString());
+     * response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+     * 
+     * return "HttpOnly cookies unset";
+     * }
+     */
 
-        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
-                .httpOnly(true)
-                .secure(false)
-                .path("/")
-                .maxAge(10 * 365 * 24 * 60 * 60)
-                .build();
-
-        // Add cookies to the response
-        response.addHeader("Set-Cookie", authTokenCookie.toString());
-        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
-
-        return "HttpOnly cookies set";
-    }
 }
